c2d5df693fdf5c7502a054187c7e87369feec38c
PDARSHIL312 <darshilp200300@gmail.com>
2024-08-31 19:51:17 +0530

    ValidationForBookForRepeatingBook

--- src/main/java/com/programming/Book/bookManager.java
+++ src/main/java/com/programming/Book/bookManager.java
@@ -1,5 +1,6 @@
 package com.programming.Book;
 
+import java.awt.print.Book;
 import java.util.*;
 
 public class bookManager {
@@ -8,7 +9,22 @@ public class bookManager {
 
     public void addBook(String title, String author, String publisher, String isbn, int price, int pages, String language, Date publishedDate, boolean occupied) {
         book bookClass = new book(title, author, publisher, isbn, price, pages, language, publishedDate, occupied);
+        validateTheEntry(bookClass);
+        checkIfBookAlreadyExist(bookClass);
         bookDetails.put(generateKey(bookClass), bookClass);
+    }
+
+
+    // validation of the attribute that it is Null Or Not
+    private void validateTheEntry(book bookClass)
+    {
+        bookClass.validate();
+    }
+
+    private void checkIfBookAlreadyExist(book bookClass)
+    {
+        if (bookDetails.containsKey(generateKey(bookClass)))
+            throw new RuntimeException("Book Already Exists");
     }
 
     public Collection<book> getAllBooks() {
@@ -18,4 +34,6 @@ public class bookManager {
     private String generateKey(book bookClass) {
         return String.format("%s-%s", bookClass.getTitle(), bookClass.getIsbn());
     }
+
+
 }
--- src/test/java/com/programming/Book/bookManagerTest.java
+++ src/test/java/com/programming/Book/bookManagerTest.java
@@ -1,9 +1,6 @@
 package com.programming.Book;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -45,12 +42,42 @@ class bookManagerTest {
     }
 
     @Test
+    @DisplayName("Here checking for the Validation and also if their null than it will throw error i use here assertThrows For that")
     public void checkThatEntryDoneHavingAnyNullOrNotAndTheValidtion()
     {
         Calendar calendar = Calendar.getInstance();
         calendar.set(2003, Calendar.DECEMBER, 3);
         Date publishedDate = calendar.getTime();
-        library.addBook("The SnowBalls", "NoOne", "TATA", "2345567890123", 10, 231, "English", publishedDate, false);
-
+        library.addBook("The SnowBall", "NoOne", "TATA", "2345567890123", 10, 231, "English", publishedDate, false);
+        assertThrows(RuntimeException.class , () ->{
+            library.addBook("The SnowBall", "NoOne", "TATA", null, 10, 231, "English", publishedDate, false);
+            library.addBook(null, "NoOne", "TATA", null, 10, 231, "English", publishedDate, false);
+            library.addBook(null, null, "TATA", null, 10, 231, "English", publishedDate, false);
+            library.addBook(null, null, null, null, 10, 231, "English", publishedDate, false);
+            library.addBook("The SnowBall", "NoOne", "TATA", "12345", 0, 231, "English", publishedDate, false);
+            library.addBook("The SnowBall", "NoOne", "TATA", "12345", 110, 0, "English", publishedDate, false);
+            library.addBook("The SnowBall", "NoOne", "TATA", "12345", 0, 231, null, publishedDate, false);
+            library.addBook("The SnowBall", "NoOne", "TATA", "12345", 0, 231, "English", null, false);
+        });
     }
+
+    @Test
+    @DisplayName("Here check that this boook is already exist if yes than throw error")
+    public void checkIfBookIsAlreadyAdded()
+    {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2003, Calendar.DECEMBER, 3);
+        Date publishedDate = calendar.getTime();
+        library.addBook("The SnowBall", "NoOne", "TATA", "2345567890123", 10, 231, "English", publishedDate, false);
+
+        assertFalse(library.getAllBooks().isEmpty(), "The library should not be empty after adding a book.");
+
+        // Here i am checking that if i agian enter the same book it Thorw exception or not
+        library.addBook("The SnowBall", "NoOne", "TATA", "2345567890123", 10, 231, "English", publishedDate, false);
+    }
+
+//    @Test
+
+
+
 }
